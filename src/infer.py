f6b575eacfdc8a55f23483359c6cee9a8a3b9519
Jongseo Yoon <js0823@gmail.com>
2018-11-20 00:24:01 -0500

    testing

--- src/infer.py
+++ src/infer.py
@@ -1,14 +1,26 @@
 import pymc3 as pm
 import theano
+import pickle
 floatX = theano.config.floatX
 from scipy.stats import mode
 
-def train_model(inference_alg, model, num_posterior, X_train, Y_train, minibatch_x=None, minibatch_y=None):
+def save_posterior(model, trace, filename):
+	with open(filename, 'wb') as buff:
+		pickle.dump({'model': model, 'trace': trace}, buff)
+	print("Saving model and trace done.")
+
+def load_posterior(filename):
+	with open(filename, 'rb') as buff:
+		data = pickle.load(buff)
+	
+	basic_model, trace = data['model'], data['trace']
+	print("Loading model and trace done.")
+	return basic_model, trace
+
+
+def train_model(inference_alg, model, num_posterior, nn_input, nn_output, X_train, Y_train):
 	#inference_alg.fit(n, method, data)
 	#return posterior_samples
-	
-	nn_input = theano.shared(X_train.astype(floatX))
-	nn_output = theano.shared(Y_train.astype(floatX))
 
 	if inference_alg is 'advi':
 		minibatch_x = pm.Minibatch(X_train.astype(floatX), batch_size=500)
@@ -17,12 +29,15 @@ def train_model(inference_alg, model, num_posterior, X_train, Y_train, minibatch
 			inference = pm.ADVI()
 			approx = pm.fit(n=50000, method=inference, more_replacements={nn_input:minibatch_x, nn_output:minibatch_y})
 			trace = approx.sample(draws=num_posterior)
+
+			ppc_train = pm.sample_ppc(trace, samples=100)
+			pred_train = mode(ppc_train['out'], axis=0).mode[0, :]
 	elif inference_alg is 'nuts':
 		with model:
 			sample_kwargs = {'cores': 1, 'init': 'auto', 'draws': num_posterior}
 			trace = pm.sample(**sample_kwargs)
-	
-	ppc_train = pm.sample_ppc(trace, samples=100)
-	pred_train = mode(ppc_train['out'], axis=0).mode[0, :]
+
+			ppc_train = pm.sample_ppc(trace, samples=100)
+			pred_train = mode(ppc_train['out'], axis=0).mode[0, :]
 	
 	return pred_train, trace
--- src/model.py
+++ src/model.py
@@ -14,7 +14,7 @@ def create_NN(n_hidden, mean, var, nn_input, nn_output, X_train, Y_train, conv=F
 		# Initialize random weights between each layer
 		init_1 = np.random.randn(X_train.shape[1], n_hidden).astype(floatX)
 		init_2 = np.random.randn(n_hidden, n_hidden).astype(floatX)
-		init_out = np.random.randn(n_hidden,10).astype(floatX)
+		init_out = np.random.randn(n_hidden, 10).astype(floatX)
 		
 		with pm.Model() as neural_network:
 			# Weights from input to hidden layer
@@ -37,7 +37,7 @@ def create_NN(n_hidden, mean, var, nn_input, nn_output, X_train, Y_train, conv=F
 			act_2 = pm.math.tanh(pm.math.dot(act_1, weights_1_2))
 			act_out = T.nnet.softmax(pm.math.dot(act_2, weights_2_out))
 			
-			out = pm.Categorical('out', act_out, observed=nn_input, total_size=Y_train.shape[0]) # IMPORTANT for minibatches
+			out = pm.Categorical('out', act_out, observed=nn_output, total_size=Y_train.shape[0]) # IMPORTANT for minibatches
         
 	return neural_network
 		

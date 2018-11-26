949238be23d8ed52f170a759ed72621849c8eb33
Edwin Jain <Edwin.Jain@tufts.edu>
2018-11-25 21:12:41 -0500

    Almost finished glue code, but not thoroughly tested

--- src/glue.py
+++ src/glue.py
@@ -5,29 +5,51 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import keras
 
+class BNN:
+    def __init__(self, path=None, num_labels=10):
+        self.num_channels = 1
+        self.image_size = 28
+        self.num_labels = num_labels
 
-def theano_to_keras(posterior_samples):
-	return [create_keras(s) for s in posterior_samples]
+        with open(path, 'rb') as f:
+        	posteriors = pickle.load(f)
+        	models = [create_model(posterior) for posterior in posteriors]
+        
+    	def average_preds(x):
+    		preds = np.mean([model.predict(x) for model in models])
+    		return tf.convert_to_tensor(preds)
 
-def create_keras(weights):
+    	self.model = keras.core.layers.Lambda(average_preds)
+    	self.model_list = models
+    
+    def predict(self, data):
+        return self.model(data)
+
+
+def create_model(weights):
 	model = Sequential()
 	num_layers = len(weights)
+	layers = []
+	
 	for i, (layer, data) in enumerate(weights.items()):
 		if i == 0:
 			input_dim = data.shape[0]
-			model.add(Dense(data.shape[1], input_dim=input_dim))
-			model.add(Activation('tanh'))
+			layers.append(Dense(data.shape[1], input_dim=input_dim))
+			layers.append(Activation('tanh'))
 		else:
 			model.add(Dense(data.shape[1]))
-			if i == num_layers - 1:
-				model.add(Activation('softmax'))
-			else:
-				model.add(Activation('tanh'))
+			if i != num_layers - 1:
+				layers.append(Activation('tanh'))
+
+	for layer in layers:
+		model.add(layer)
+	
+	model.set_weights(weights.values())
 	return model
 
 
-if __name__ == '__main__':
-	with open('example_trace.pkl', 'rb') as f:
-		trace = pickle.load(f)
-		models = theano_to_keras(trace)
-		print(models)
+# if __name__ == '__main__':
+# 	with open('example_trace.pkl', 'rb') as f:
+# 		trace = pickle.load(f)
+# 		models = theano_to_keras(trace)
+# 		print(models)
